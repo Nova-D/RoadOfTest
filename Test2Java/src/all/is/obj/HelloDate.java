e388426a0c562a43816da1480c43e16e8a88f145
Nova Ding <nova.dzy@gmail.com>
2015-11-13 21:03:46 +0800

    First Git File.

---
+++ Test2Java/src/all/is/obj/HelloDate.java
@@ -0,0 +1,10 @@
+package all.is.obj;
+
+import java.util.Date;
+
+public class HelloDate {
+	public static void main(String[] args){
+		System.out.println("Hello,this is my first JavaDemo.");
+		System.out.println("Current Time is: " + new Date());
+	}
+}
